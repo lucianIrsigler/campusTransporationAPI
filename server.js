8ec94863566473ec027608dc0a863e80def24456
lucianIrsigler <l.irsigler64@gmail.com>
2024-08-21 13:31:06 +0200

    updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const YAML = require('yamljs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Load Swagger YAML file
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
