cc06b4c4516121b7e5007170379889683ab0d7ab
lucianIrsigler <l.irsigler64@gmail.com>
2024-08-21 13:20:07 +0200

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Load Swagger YAML file
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
+
+// Set up Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/api-docs`);
+});
